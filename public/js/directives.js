34c6965e419a0235ef05f4a3f47bcd15cd09467c
aarosil <siladi@gmail.com>
2014-02-23 14:38:10 -0800

    Cloud pie chart directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -42,7 +42,7 @@ weatherDirectives.directive('weatherLineChart', ['d3Service', '$window',
 						
 						if(!data) return;						
 						
-						var elemWidth = d3.select('svg').node().offsetWidth||500;
+						var elemWidth = svg.node().offsetWidth;
 						var margin = {top: 20, right: 20, bottom: 30, left: 50},
 						    width =  parseInt(elemWidth,10) - margin.left - margin.right,
 						    height = 200 - margin.top - margin.bottom;
@@ -106,3 +106,91 @@ weatherDirectives.directive('weatherLineChart', ['d3Service', '$window',
 		}
 	}]);
 
+weatherDirectives.directive('weatherPieChart', ['d3Service', '$window', 
+	function(d3Service, $window) {
+		return {
+			restrict: 'EA', 
+			scope: {
+				data: '='
+			},
+			link: function(scope, ele, attrs) {
+				d3Service.d3().then(function(d3) {
+					
+					var svg = d3.select(ele[0])
+						.append('svg:svg')
+						.style('width', '100%');
+
+					window.onresize = function() {
+						scope.$apply();
+					}
+					
+					scope.$watch(function(){
+						return angular.element($window)[0].innerWidth;
+					}, function() {
+						scope.render(scope.data);
+					});
+		
+					scope.$watch('data', function(newVals, oldVals) {
+					  return scope.render(newVals);
+					}, true);	
+
+					scope.render = function(data) {
+						
+						svg.selectAll('*').remove();
+
+						if(!data) return;						
+
+						//data = [ 
+						//data is the % cloudy daylight hours 
+						//	{status: "Cloudy" , value: 100 * data},
+						//	{status: "Clear" , value: 100 * (1-data)}
+						//];
+
+						var elemWidth = svg.node().offsetWidth;
+						//var elemWidth = 700
+						//console.log(d3.select(ele[0]).node())
+						//console.log('elemWidth: ' + elemWidth)
+						var width =  elemWidth;
+						var height = 200 ;					
+						var radius = Math.min(width, height) / 2;
+
+						var color = d3.scale.ordinal()
+							.range(["#98abc5", "#8a89a6"]);
+
+						var arc = d3.svg.arc()
+							.outerRadius(radius - 10)
+							.innerRadius(0);
+
+						var pie = d3.layout.pie()
+							.sort(null)
+							.value(function(d) { return d.value; });
+
+						console.log('new width: ' + width + ' height: ' + height )
+						
+						svg.attr("width", parseInt(width, 10))
+							.attr("height", height)
+
+						var chart = svg.append("g")
+							.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+						var g = chart.selectAll(".arc")
+							.data(pie(data))
+							.enter().append("g")
+							.attr("class", "arc");
+
+						g.append("path")
+							.attr("d", arc)
+							.style("fill", function(d) { return color(d.data.status); });
+
+						g.append("text")
+							.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+							.attr("dy", ".35em")
+							.style("text-anchor", "middle")
+							.text(function(d) { return d.data.status; });
+
+					}					
+
+				})
+			}
+		}
+	}]);
