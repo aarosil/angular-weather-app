11d876d4b8987d6b1be0965b9b99314e0149f1ea
aarosil <siladi@gmail.com>
2014-03-07 11:31:02 -0800

    Cleanup wuResponse function

--- app/apiclient/wuResponse.js
+++ app/apiclient/wuResponse.js
@@ -1,120 +1,124 @@
 var _ = require('underscore');
 var SunCalc = require('suncalc');
 
-var wuResponse = {}
+var wuResponse = function (data, fields, location, summaryFields) {
 
-// function to completely handle the responses from Weather underground
-wuResponse.processData = function(data, fields, location, summaryFields) {
-    // data is the current observation 
-    if (typeof data === 'object' && data.hasOwnProperty('current_observation')) {
-        return _.pick(data.current_observation, 'weather', 'temp_f', 'temp_c', 'relative_humidity', 'wind_string', 'dewpoint_string')
-    }
+	return init()
 
-	if (!data||!fields||!location) return;
+	function init() {
+		if (!data) {return}
+		if (data.hasOwnProperty('current_observation')) {
+			return processCurrent(data.current_observation)
+		}
+		if (!fields||!location) {return;}
+		if (data instanceof Array && data.length > 0 ) {
+			return processHistory(data)
+		} 
+	}
 
-	// data is a single day's collection of observations
-    if (typeof data === 'object' && data.hasOwnProperty('history') && data.history.observations.length > 0) {
+	function processCurrent(current) {
+		return _.pick(current, 'weather', 'temp_f', 'temp_c', 'relative_humidity', 'wind_string', 'dewpoint_string')
+	}
 
-		var observations = data.history.observations;
+	function processDay(day) {
 		var response = { 
-			//add master date to allow sorting/concatting of async data before response
-			date: new Date(observations[0].date.year, parseInt(observations[0].date.mon,10)-1, observations[0].date.mday, 12,0,0,0,0),
-			//values for all observations
+			// add master date to allow sorting/concatting of async data before response
+			date: new Date(day[0].date.year, parseInt(day[0].date.mon,10)-1, day[0].date.mday, 12,0,0,0,0),
+			// extracted values for all the observations
 			observations: [],
-			//any summaries for the response
+			// any summary averages calculated for the response
 			summaryData: {},
 			summaryFields: []
 		}
+
         // get daylight hours
         var coords = location.split(",")
         var daylight = SunCalc.getTimes(response.date, coords[0], coords[1]);
         // counter for # daytime cloudy observations
         var dayObs = 0;
         var dayCloudyObs = 0;
-        // hold totals if any summary requested
-        // count # obs. in case any were invalid
-        var total = {};
-        var count = {};
-		//go through all observations for the day
-		_.each(observations, function(obs){
-			var entry = {};
-			var value;
-			entry.time = new Date(obs.date.year, parseInt(obs.date.mon, 10)-1, obs.date.mday, obs.date.hour, obs.date.min).getTime();
-			
-			//add each field as a property of the entry
+        // initialize obj to accumulate totals of any summary avgs requested
+        // and count the # obs. in case any were invalid (ie -999)
+        var totals = {};
+		_.each(summaryFields, function(field){
+			totals[field] = {count: 0, value: 0}
+		})
+
+		// go through all observations for the day		
+		_.each(day, function(obs){
+			var entry = {time: new Date(obs.date.year, parseInt(obs.date.mon, 10)-1, obs.date.mday, obs.date.hour, obs.date.min).getTime()};
+			var value;			
+
+			// add each field as a property of the entry
 			_.each(fields, function(field){
-				if (field === 'conds') { 
-					// map condition value to equivalent cloud coverage
-					value = wuSkyConditionsMap(obs[field]) 
-                    // increment daytime cloudiness counters
-                    if ( daylight.sunriseEnd.getTime() < entry.time && entry.time < daylight.sunset.getTime() ) {
-                        dayObs++;
-                        dayCloudyObs += value;
-                    }
-				}  else {
-					value = reject(obs[field]) ? 0 : Number(obs[field]);
-				}
-				entry[field] = value;
-			})
-			
-			// add to totals for each summary fields 
-			_.each(summaryFields, function(field){
-				if (field === 'conds') { 
-					value = wuSkyConditionsMap(obs[field]) 
-				} else {
-				value = reject(obs[field]) ? 0 : Number(obs[field]);
-				}
-				if (!total[field]) {
-					total[field]=0 
-					count[field]=0
-				}
-				total[field] += value;
-				count[field] += 1;										
+				entry[field] = extractValue(obs,field)
 			})
 
+			// accumulate avgs but do not cound bad obs. 
+			// values toward the summary averages
+			_.each(summaryFields, function(field){
+				if (reject(obs[field]) === false) {
+					totals[field].value += extractValue(obs,field);
+					totals[field].count += 1;	
+				}
+			})	
+
+	        // if this observation occured in daylight, 
+	        // accumulate towards total daytime cloudiness
+	        if ( daylight.sunriseEnd.getTime() < entry.time && daylight.sunset.getTime() > entry.time) {
+	            dayObs++;
+	            dayCloudyObs += extractValue(obs, 'conds');
+	        }
+
 			response.observations.push(entry)
-		})	
+
+		})
 
 		// calcuate any summaries, reported once a day
         response.summaryData.time = response.date.getTime()
-        _.each(summaryFields, function(field){
-        	var name = 'avg_' + field
-        	response.summaryFields.push(name)
-        	response.summaryData[name] = total[field]/count[field]
+        response.summaryFields = summaryFields.map(function(i){return 'avg_' + i})
+        _.each(_.keys(totals), function(key){
+        	response.summaryData['avg_'+key] = totals[key].value/totals[key].count
         })
+
         // calculate avg daytime cloudiness for the day. 
         var avgClouds = (dayCloudyObs/dayObs)||0        
         response.summaryFields.push('day_conds')
         response.summaryData.day_conds = Number(avgClouds.toFixed(1));
 		return response;	
- 	}
 
-    // data is an array of daily observations responses
-	if ( data instanceof Array && data.length > 0 ) {
-        var masterResponse = [];
-        var masterSummaryData = [];
-        var masterSummaryFields;
-        // so process them individually as above 
-        var processed = _.map(data, function(r) { return wuResponse.processData(r, fields, location, summaryFields); })
+	}
+
+	function processHistory(history) {
+        var response = {
+			fields: fields, 
+			summaryFields: [],
+			observations: [], 
+			summary: []        	
+        }
+        // process each of the days within 'history'
+        var processed = _.map(history, function(day) { return processDay(day.history.observations); })
         // sort the results by date
-		processed = _.sortBy(processed, function(item){return item.date})
+		processed = _.sortBy(processed, function(day){return day.date})
 		//then concat into one master dataseries representing all requested days.
         _.each(processed, function(resp){
-        	masterResponse = masterResponse.concat(resp.observations)
-        	masterSummaryData.push(resp.summaryData)
-        	masterSummaryFields = resp.summaryFields
+        	response.observations = response.observations.concat(resp.observations)
+        	response.summary.push(resp.summaryData)
+        	response.summaryFields = resp.summaryFields
 		})
-		return {
-			fields: fields, //return the fields for frontend easiness
-			summaryFields: masterSummaryFields,
-			observations: masterResponse, 
-			summary: masterSummaryData
-		};
+		return response
 	}
 
-	else {
-		return;
+	function extractValue(obj, key) {
+		if (key === 'conds') {return wuSkyConditionsMap(obj[key]);}
+		return reject(obj[key]) ? 0 : Number(obj[key]);
 	}
+
+	function reject(value) {
+		if (typeof value === 'undefined' || value === null) { return true }		
+		var re = /N\/A|-9999|-999/
+		return re.test(value);
+	}	
 
     function wuSkyConditionsMap(e) {
         if (e === 'Mostly Cloudy') return 75;
@@ -126,14 +130,6 @@ wuResponse.processData = function(data, fields, location, summaryFields) {
         return 100;
     }
 
-	// ignore undefined value and some 
-	// weatherstations reporting "-9999.00" for unknowns
-	function reject(value) {
-		if (typeof value === 'undefined' || value === null) { return true }		
-		var re = /N\/A|-9999|-999/
-		return re.test(value);
-	}
-
 }
 
-module.exports = wuResponse.processData
+module.exports = wuResponse
--- test/unit/node/wuResponseSpec.js
+++ test/unit/node/wuResponseSpec.js
@@ -16,33 +16,50 @@ var location  = '37,-121';
 var processed;
 
 describe('Weather Underground Response Handler', function(){
+/**
+Process historical observations and return data as such:
+{
+fields: ['field1', 'field2'],
+summaryFields: ['avg_field1', 'avg_field2'],
+observations: [
+	{time: 1, field1: 50, field2: 70},
+	{time: 2, field1: 50, field2: 70},
+	...
+] ,
+summary: [
+	{time: 1, avg_field1: 75, avg_field2: 66},
+	{time: 2, avg_field1: 71, avg_field2: 57},
+	...
+],
+}
+**/
 
 	it('should calculate the average daytime cloud cover properly', function(){
 		// only daytime observations should influence day_conds
 		summaryFields = ['tempi', 'conds', 'wspdi'];
 		fields = ['tempi', 'conds', 'wspdi'];
-		processed = wuResponse(data.feb26, fields, location, summaryFields)
+		processed = wuResponse([data.feb26], fields, location, summaryFields)
 		expect(processed.summaryData).not.toBe(null)
-		expect(processed.summaryData.avg_wspdi).toBe(30)
-		expect(processed.summaryData.avg_tempi).toBe(50)
+		expect(processed.summary[0].avg_wspdi).toBe(30)
+		expect(processed.summary[0].avg_tempi).toBe(50)
 		// all daytime observations were cloudy
-		expect(processed.summaryData.day_conds).toBe('100.0')
+		expect(processed.summary[0].day_conds).toBe(100)
 		// only 3/4 total observations were cloudy
-		expect(processed.summaryData.avg_conds).toBe(75)
+		expect(processed.summary[0].avg_conds).toBe(75)
 		
 	})
 
 	it('should calculate the other averages properly, ignoring bad values in calculating the avg', function(){
 		fields = ['tempi', 'conds', 'wspdi', 'hum'];
 		summaryFields = ['tempi', 'conds', 'wspdi', 'hum'];
-		processed = wuResponse(data.dayWithBadTemps, fields, location, summaryFields)
-		expect(processed.summaryData.avg_tempi).toBe(50)
-		expect(processed.summaryData.avg_conds).toBe(25)
-		expect(processed.summaryData.avg_wspdi).toBe(30)
-		expect(processed.summaryData.avg_hum).toBe(70)
+		processed = wuResponse([data.dayWithBadTemps], fields, location, summaryFields)
+		expect(processed.summary[0].avg_tempi).toBe(50)
+		expect(processed.summary[0].avg_conds).toBe(25)
+		expect(processed.summary[0].avg_wspdi).toBe(30)
+		expect(processed.summary[0].avg_hum).toBe(70)
 	})
 
-	// behavior when called with an array of response objects
+	// result has observation from both days 
 	it('should process an array of "history" responses and return the fields as expected', function(){
 		fields = ['tempi', 'conds', 'wspdi'];
 		processed = wuResponse([data.feb27,data.feb26], fields, location, summaryFields)
@@ -52,7 +69,7 @@ describe('Weather Underground Response Handler', function(){
 
 	//generic behaviors
 	it('should remove improper values such as N/A , -9999, and -999', function() {
-		processed = wuResponse(data.allBadValues, ['tempi','dewpti','humi'], location, summaryFields)
+		processed = wuResponse([data.allBadValues], ['tempi','dewpti','humi'], location, summaryFields)
 		expect(processed.observations[0].tempi).toBe(0)
 		expect(processed.observations[0].dewpti).toBe(0)
 		expect(processed.observations[0].humi).toBe(0)
